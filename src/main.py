8f86eeb7a13f0078b77e4f528abfa767de27d102
Sungchul Hong <chulhongsung@gmail.com>
2024-09-06 12:56:28 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -88,8 +88,8 @@ def evaluate(model, loader, criterion, device):
     
 def main():
     
-    df_train_total = pd.read_csv("./data/df_train_total.csv")
-    df_merged = pd.read_csv("./data/df_merged.csv")
+    df_train_total = pd.read_csv("../data/df_train_total.csv")
+    df_merged = pd.read_csv("../data/df_merged.csv")
     
     train_conti_input, train_cate_input, train_future_input, train_label = generate_ts_data(df_train_total, df_merged, input_seq_len=args.seq_len, tau=args.tau)
 
